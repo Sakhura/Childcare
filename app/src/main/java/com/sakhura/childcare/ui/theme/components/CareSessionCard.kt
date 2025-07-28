7e4f8a94675f0348948066c2110aed23510f2bd8
SoloLeveling-dev25 <edutecnosabina@gmail.com>
2025-07-28 18:18:48 -0400

    se agregan

--- app/src/main/java/com/sakhura/childcare/ui/theme/components/CareSessionCard.kt
+++ app/src/main/java/com/sakhura/childcare/ui/theme/components/CareSessionCard.kt
@@ -1,4 +1,2 @@
 package com.sakhura.childcare.ui.theme.components
 
-class CareSessionCard {
-}
--- app/src/main/java/com/sakhura/childcare/ui/theme/screens/HomeScreen.kt
+++ app/src/main/java/com/sakhura/childcare/ui/theme/screens/HomeScreen.kt
@@ -1,2 +1,52 @@
 package com.sakhura.childcare.ui.theme.screens
 
+import androidx.compose.material3.Scaffold
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeScreen(
+    navController: NavController,
+    viewModel: ChildcareViewModel()
+){
+    val children by viewModel.children.collectAsState(initial = emptyList()
+        val activeSession by viewModel.activeSession.collectAsState(initial = null)
+
+    Scaffold (
+        topBar = {
+            TopAppBar(
+                title = { Text(text = "Childcare App") },
+                colors = TopAppBarDefaults.smallTopAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primary,
+                    titleContentColor = MaterialTheme.colorScheme.onPrimary
+                )
+            )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = { navController.navigate("add_child") }
+            ) {
+                Icon(imageVector = Icons.Default.Add, contentDescription = "Add Child")
+            }
+        }
+    ) { paddingValues ->
+        LazyColumn(
+            modifier = Modifier
+                .padding(paddingValues)
+                .fillMaxSize()
+            verticalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            activeSession?.let {
+                item {
+                    ActiveSessionCard(session = it)
+                }
+            }
+            items(children) { child ->
+                ChildCard(
+                    child = child
+                    onClick = { navController.navigate("child_detail/${child.id}") }
+                    onStarCare = { viewModel.startCareSession(child.id) }
+                )
+            }
+        }
+        )
+}
